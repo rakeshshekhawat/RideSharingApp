66b22cf0a762c265d72b472171a7a2791b879236
rakeshshekhawat <rsshekhawat04@gmail.com>
2025-06-06 18:23:33 +0530

    updated

--- src/org/rigesharing/designPattern/BehaviouralPatterns/StrategyPattern/Exercise.java
+++ src/org/rigesharing/designPattern/BehaviouralPatterns/StrategyPattern/Exercise.java
@@ -5,112 +5,87 @@ package org.rigesharing.designPattern.BehaviouralPatterns.StrategyPattern;
 import java.util.Scanner;
 
 public class Exercise {
-    
-  
-    public void run() {
-        
-        Scanner sc = new Scanner(System.in);
-        Document document = new Document();
-        document.setFormatter(new PlainTextFormatter());
-        String userInput = sc.nextLine();
-        document.setContent(userInput);
 
-        // Using PlainTextFormatter
-        
-        
-        System.out.println("Plain Text:");
-        document.display();
+	public void run() {
 
-        // Using HTMLFormatter
-        // TODO: Set the formatter for the document to HTMLFormatter.
-        
-        
-        System.out.println("HTML Format:");
-        document.display();
+		Scanner sc = new Scanner(System.in);
+		Document document = new Document();
+		document.setFormatter(new PlainTextFormatter());
+		String userInput = sc.nextLine();
+		document.setContent(userInput);
 
-        // Using MarkdownFormatter
-        // TODO: Set the formatter for the document to MarkdownFormatter.
-        
-        
-        System.out.println("Markdown Format:");
-        document.display(); 
-        
-        sc.close();
-    }
-    public static void main (String args[]) {
-    	Document doc = new Document();
-    	Exercise exercise = new Exercise();
-    	exercise.run();
-    	
-    }
+		document.display();
+		sc.close();
+	}
+
+	public static void main(String args[]) {
+		Exercise exercise = new Exercise();
+		exercise.run();
+
+	}
 }
 
-interface formatter{
+interface formatter {
 
-    void format();
-    void setContent(String content);
-}
-class Document{
-    formatter formatter;
-    public void setFormatter(formatter formatter){
-        this.formatter=formatter;
-    }
-    
-   
-    public void display() {
-    	formatter.format();
-    }
-    public void setContent(String content){
-        this.formatter.setContent(content);
-    }
-    
-    
-    
+	void format();
+
+	void setContent(String content);
 }
 
-class HTMLFormatter implements formatter{
-    String content;
-    public void format(){
-        System.out.println("This is HTMLFormatter :"+content );
-    }
-    public void setContent(String content){
-        this.content=content;
-    }
+class Document {
+	formatter formatter;
+
+	public void setFormatter(formatter formatter) {
+		this.formatter = formatter;
+	}
+
+	public void display() {
+		formatter.format();
+	}
+
+	public void setContent(String content) {
+		this.formatter.setContent(content);
+	}
+
 }
 
-class PlainTextFormatter implements formatter{
-    String content;
-    public void format(){
-        System.out.println("This is PlainTextFormatter :"+content );
-    }
-    public void setContent(String content){
-        this.content=content;
-    }
+class HTMLFormatter implements formatter {
+	String content;
+
+	public void format() {
+		System.out.println("This is HTMLFormatter :" + content);
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
-class MarkdownFormatter implements formatter{
-    String content;
-    public void format(){
-        System.out.println("This is MarkdownFormatter :"+content );
-    }
-    public void setContent(String content){
-        this.content=content;
-    }
-    
+
+class PlainTextFormatter implements formatter {
+	String content;
+
+	public void format() {
+		System.out.println("This is PlainTextFormatter :" + content);
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
+
+class MarkdownFormatter implements formatter {
+	String content;
+
+	public void format() {
+		System.out.println("This is MarkdownFormatter :" + content);
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
 }/*
 	 * class Test{ public static void main(String args[]) {
 	 * 
 	 * } }
 	 */
-
-
-
-
-
-
-
-
-
-
-
-
-
